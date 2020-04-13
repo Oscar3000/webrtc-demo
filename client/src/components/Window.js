06dc1d5b53d89cb4fbf6b9a0d57438d0de450104
Umeh Oscar <oscar@dbbsoftware.com>
2020-04-13 19:23:53 +0300

    changed id generator

--- client/src/components/Window.js
+++ client/src/components/Window.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import socket from "../utils/socket";
 import "./Window.css";
 
 function Window() {
+    const [clientId, setClientId] = useState(null);
+    const [callFrom, setCallFrom] = useState(null);
+    useEffect(()=>{
+        socket.on('init', ({ id: clientId})=>{
+            console.log(`Client connected by ${clientId}`);
+            setClientId(clientId);
+        })
+        .on('request', ({ from: callFrom}) => {
+            console.log(`call coming from ${callFrom}`);
+            setCallFrom(callFrom);
+        })
+        .emit('init');
+    }, [])
     return (
         <div className="container">
             In the window component.
--- client/src/utils/mediaDevice.js
+++ client/src/utils/mediaDevice.js
@@ -1,19 +1,66 @@
-export default class MediaDevice {
- constructor(){
-     super();
-     if(hasUserMedia()) {
+import _ from "lodash";
 
-     }else {
-         alert("This browser doesn't support webRtc");
-     }
- }
+export default class MediaDevice {
+  /**
+   * Start media devices and send stream
+   */
+  start() {
+      const constraints = {
+          video: {
+              facingMode: 'user',
+              height: {
+                  min: 360,
+                  ideal: 720,
+                  max: 1080
+              }
+          },
+          audio: true
+      };
+
+      if (hasUserMedia()) {
+          navigator.
+          mediaDevices.getUserMedia(constraints)
+          .then((stream) => {
+              this.stream = stream;
+              this.emit('stream', stream);
+          })
+    } else {
+      alert("This browser doesn't support webRtc");
+    }
+    return this;
+  }
+
+  /**
+   * Turn on/off a device
+   */
+  toggle(type,on) {
+      const len = arguments.length;
+      if(this.stream) {
+          this.stream[`get${type}Tracks`]()
+          .forEach(track => {
+              const state = len === 2 ? on : !track.enabled;
+              _.set(track, 'enabled',state);
+          });
+      }
+      return this;
+  }
+
+  /**
+   * Stop all media track of devices
+   */
+  stop() {
+      if(this.stream) {
+          this.stream.getTracks().forEach(track => track.stop());
+      }
+      return this;
+  }
 }
 
 function hasUserMedia() {
-    navigator.getUserMedia =
-      navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia;
-    return !!navigator.getUserMedia;
-  }
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+  return !!navigator.getUserMedia;
+}
